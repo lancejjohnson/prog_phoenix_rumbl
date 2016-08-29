84133e9f153159b18b6a94db1eab21f725650c11
Lance Johnson <lancejjohnson@gmail.com>
2016-08-29 17:28:36 -0400

    Speed up tests by softening crypto requirements in test env

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -5,7 +5,7 @@ defmodule Rumbl.TestHelpers do
     changes = Map.merge(
       %{
         name: "Some User",
-        username: "user#{Base.encode16(:crypto.rand_bytes(8))}",
+        username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
         password: "supersecret"
       },
       attrs
