baefe39cb18c9031d3cbff3203af51e4cb8646a8
Lance Johnson <lancejjohnson@gmail.com>
2016-08-30 09:17:07 -0400

    Add view tests
    The most important concept here: templates are functions of the parent's
    view.

---
+++ test/views/video_view_test.exs
@@ -0,0 +1,35 @@
+defmodule Rubml.VideoViewTest do
+  use Rumbl.ConnCase, async: true
+  import Phoenix.View
+  alias Rumbl.Video
+
+  test "renders index.html", %{conn: conn} do
+    videos = [
+      %Video{id: "1", title: "dogs"},
+      %Video{id: "2", title: "cats"},
+    ]
+
+    # NOTE: The final arg is a keyword-list
+    content = render_to_string(Rumbl.VideoView, "index.html", conn: conn, videos: videos)
+
+    assert String.contains?(content, "Listing videos")
+    for video <- videos do
+      assert String.contains?(content, video.title)
+    end
+  end
+
+  test "renders new.html", %{conn: conn} do
+    changeset = Video.changeset(%Video{})
+    categories = [{"cats", 123}]
+
+    content = render_to_string(
+      Rumbl.VideoView,
+      "new.html",
+      conn: conn,
+      changeset: changeset,
+      categories: categories
+    )
+
+    assert String.contains?(content, "New video")
+  end
+end
